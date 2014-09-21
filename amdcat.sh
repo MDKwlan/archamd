27b9607dc74e7afe8cfcc4a012b66be6fa8b7772
MDK <mdkwlan@gmail.com>
2014-09-21 15:07:50 -0500

    Files
    New mkinitcpio.conf pacman.conf and radeon.conf. All are dependencies of
    amdcat.sh

---
+++ amdcat.sh
@@ -0,0 +1,10 @@
+cp pacman.conf /etc/pacman.conf
+cp radeon.conf /etc/modules-load.d/
+cp mkinitcipio.conf /etc/mkinitcipio.conf
+pacman-key -r 653C3094
+pacman-key --lsign-key 653C3094
+pacman -Syy
+pacman -S catalyst-hook catalyst-utils catalyst-libgl opencl-catalyst lib32-catalyst-utils lib32-catalyst-libgl lib32-opencl-catalyst
+aticonfig --initial
+systemctl enable catalyst-hook
+systemctl start catalyst-hook
