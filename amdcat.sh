e2c2c713f256f3c3c67288bd2da4aac349f52acc
MDK <mdkwlan@gmail.com>
2014-12-04 16:08:07 -0600

    Update amdcat.sh

--- amdcat.sh
+++ amdcat.sh
@@ -2,7 +2,6 @@
 
 cp pacman.conf /etc/pacman.conf #This pacman.conf has multilib xorg115 and catalysts repo
 cp radeon.conf /etc/modules-load.d/ #This will add the blacklist for the radeon FOSS driver and add fglxr to start
-cp mkinitcpio.conf /etc/mkinitcpio.conf #This will add nomodeset to disable KMS.
 pacman-key -r 653C3094 #Vi0l0s key for verifying the new repos added in pacman.conf
 pacman-key --lsign-key 653C3094 #This is mainly a backup if -r fails.
 pacman -Syy #Updating all the new repos that have been added
